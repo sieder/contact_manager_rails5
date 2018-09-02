cdf79499673b37af8efcc24f3fd63f951acead88
Sieder Villareal <siedervillareal@JUMP030-SVILLAREAL.local>
2018-09-02 23:01:16 +0800

    sidebar link functional

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,10 @@
 class ContactsController < ApplicationController
 	def index
-		@contacts = Contact.page(params[:page])
+		if params[:group_id] && !params[:group_id].empty?
+			@contacts = Group.find(params[:group_id]).contacts.page(params[:page])
+		else
+			@contacts = Contact.page(params[:page])
+		end
 	end
 
 
